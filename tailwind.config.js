ee3254e805c667dc839e09b4eb7144608968f45a
b-luis <bscarletluis@gmail.com>
2023-08-30 12:15:32 +0800

    feat: tailwind css configuration - font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,17 @@ export default {
 	theme: {
 		extend: {
 			colors: {
+				primary: "#F5F7FB",
 				delft: {
 					DEFAULT: "#293264",
 					dark: "#4D5B9E",
 					light: "#D6DBF5",
 					soft: "#DBDEF0"
 				},
-				primary: "#F5F7FB",
 				"razzle-rose": {
 					DEFAULT: "#e118d4",
 					700: "#bb10ac",
-					800: "#98108a",
-					900: "#7c136f",
-					950: "#54004a"
+					800: "#98108a"
 				}
 			},
 			borderRadius: {
