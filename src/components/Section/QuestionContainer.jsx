c90fb3d7f6538b15fbaaee27c98dee4c248710e6
b-luis <bscarletluis@gmail.com>
2023-08-31 18:05:11 +0800

    fix: section responsiveness in mobile

--- src/components/Section/QuestionContainer.jsx
+++ src/components/Section/QuestionContainer.jsx
@@ -1,6 +1,6 @@
 function QuestionContainer({ children }) {
 	return (
-		<div className="align-center relative mx-auto flex h-screen w-full flex-col justify-center overflow-hidden text-delft md:w-[720px] ">
+		<div className="align-center relative mx-auto flex w-full  flex-col justify-center overflow-hidden text-delft sm:h-screen md:w-[720px] ">
 			<section className=" rounded-lg border-2 border-delft-light p-5 shadow-lg ">
 				<h2>
 					<span className="font-bold">Directions: </span>Choose the correct answer from the options
